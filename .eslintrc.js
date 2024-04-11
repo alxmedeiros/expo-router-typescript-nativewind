0310213d3fa04d1a31917d005336d5f87e7a0ab2
Alexandre Medeiros <alxmedeiros@gmail.com>
2024-04-11 00:05:38 -0300

    Publishing Expo template

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: [
+    'universe/native',
+    'universe/shared/typescript-analysis',
+  ],
+  overrides: [
+    {
+      files: [
+        '*.ts',
+        '*.tsx',
+        '*.d.ts'
+      ],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+    },
+  ],
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
